3c13d0f11d1298fa383a1b88c10155507d97a0e7
Rungbam(main) <marungbam@naver.com>
2023-11-15 20:57:29 +0900

    aiming_point가 마우스 커서를 따라가게함

--- aiming_point.py
+++ aiming_point.py
@@ -1,37 +1,23 @@
-from pico2d import get_time, load_image, load_font, clamp, draw_rectangle
-from sdl2 import SDL_MOUSEBUTTONDOWN, SDL_BUTTON_LEFT, SDL_MOUSEBUTTONUP
+from pico2d import get_time, load_image, load_font, clamp, draw_rectangle, get_events
+from sdl2 import SDL_MOUSEBUTTONDOWN, SDL_BUTTON_LEFT, SDL_MOUSEMOTION
 
 import game_world
 import game_framework
 
-def left_click_down(e):
-    return e[0] == 'INPUT' and e[1].type == SDL_MOUSEBUTTONDOWN and e[1].button == SDL_BUTTON_LEFT
-
-def left_click_up(e):
-    return e[0] == 'INPUT' and e[1].type == SDL_MOUSEBUTTONUP and e[1].button == SDL_BUTTON_LEFT
-
-def time_out(e):
-    return e[0] == 'TIME_OUT'
-
-
-class Aiming_point_move:
-    pass
-
-
-class Bang:
-    pass
-
-
-class StateMachine:
-    pass
+# def left_click_down(e):
+#     return e[0] == 'INPUT' and e[1].type == SDL_MOUSEBUTTONDOWN and e[1].button == SDL_BUTTON_LEFT
+#
+# def left_click_up(e):
+#     return e[0] == 'INPUT' and e[1].type == SDL_MOUSEBUTTONUP and e[1].button == SDL_BUTTON_LEFT
+#
+# def time_out(e):
+#     return e[0] == 'TIME_OUT'
 
 
 class Aiming_point:
     def __init__(self):
         self.x, self.y = 400, 300
         self.image = load_image('aiming_point.png')
-        self.state_machine = StateMachine(self)
-        self.state_machine.start()
         self.bullet_count = 6
 
 
@@ -41,14 +27,20 @@ class Aiming_point:
             # 마우스 좌표 저장?
             # 충돌 체크(마우스 좌표와 clay_plate 간)
 
-    def update(self):
-        self.state_machine.update()
+    def update(self, event=None):
+        events = get_events()
+        for event in events:
+            if event.type == SDL_MOUSEMOTION:
+                self.x, self.y = event.x, 600 - 1 - event.y
+            if event.type == SDL_MOUSEBUTTONDOWN:
+                if event.button == SDL_BUTTON_LEFT:
+                    self.x, self.y = event.x, 600 - 1 - event.y
 
     def handle_event(self, event):
-        self.state_machine.handle_event(('INPUT', event))
+        pass
 
     def draw(self):
-        pass
+        self.image.clip_draw(0, 0, 512, 512, self.x, self.y, 50, 50)
 
     def get_bb(self):
         pass
