9b512adfc596b16e7c2a70c7d388075c9db41633
Rungbam(main) <marungbam@naver.com>
2023-11-15 22:45:09 +0900

    커서 숨기기

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from pico2d import open_canvas, delay, close_canvas
+from pico2d import open_canvas, delay, close_canvas, hide_cursor
 import game_framework
 
 import play_mode as start_mode
 
 open_canvas(800, 600)
+hide_cursor()
 game_framework.run(start_mode)
 close_canvas()
