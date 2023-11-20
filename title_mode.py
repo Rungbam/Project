02c423c11de8a308fab1abe837bec25af7261984
Rungbam(main) <marungbam@naver.com>
2023-11-20 22:56:09 +0900

    title_mode 추가

--- title_mode.py
+++ title_mode.py
@@ -1,5 +1,4 @@
-import title_mode
-from pico2d import load_image, get_events, clear_canvas, update_canvas, get_time
+from pico2d import load_image, get_events, clear_canvas, update_canvas
 from sdl2 import SDL_QUIT, SDL_KEYDOWN, SDLK_ESCAPE
 
 import game_framework
@@ -7,7 +6,7 @@ import game_framework
 
 def init():
     global image
-    image = load_image('tuk_credit.png')
+    image = load_image('title.png')
 
 def finish():
     global image
@@ -15,19 +14,11 @@ def finish():
 
 def handle_events():
     events = get_events()
-    for event in events:
+    for event in events():
         if event.type == SDL_QUIT:
             game_framework.quit()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             game_framework.quit()
-
-
-def update():
-    global logo_start_time
-    if get_time() - logo_start_time >= 2.0:
-        logo_start_time = get_time()
-        game_framework.change_mode(title_mode)
-
 
 def draw():
     clear_canvas()
