29441c491cc1ec36564a4d52f3f9bd680593ba30
Rungbam(main) <marungbam@naver.com>
2023-11-24 21:34:51 +0900

    rename play_mode
    기존 사격용 플레이 모드의 이름을 직관적으로 바꿨다

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from pico2d import open_canvas, delay, close_canvas, hide_cursor
 import game_framework
 
-import play_mode as start_mode
+import play_mode_shooting as start_mode
 # import logo_mode as start_mode
 
 open_canvas(800, 600)
