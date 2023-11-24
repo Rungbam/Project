722746c454c713ef3e2f145d8b77d8771502cf75
Rungbam(main) <marungbam@naver.com>
2023-11-24 22:04:30 +0900

    Update play_mode_hurdle.py

--- play_mode_hurdle.py
+++ play_mode_hurdle.py
@@ -1,12 +1,8 @@
-import random
-
 from pico2d import *
 import game_framework
 
 import game_world
-from field import Field
-from clay_plate import Clay_plate
-from aiming_point import Aiming_point
+from runner import Runner
 
 def handle_events():
     events = get_events()
@@ -15,22 +11,16 @@ def handle_events():
             game_framework.quit()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             game_framework.quit()
+        else:
+            runner.handle_event(event)
 
 
 def init():
+    global runner
 
-    field = Field()
-    game_world.add_object(field, 0)
-
-    aiming_point = Aiming_point()
-    game_world.add_object(aiming_point, 1)
-    game_world.add_collision_pair('aiming_point:clay_plate', aiming_point, None)
-
-    clay_plates = [Clay_plate() for _ in range(6)]
-    game_world.add_objects(clay_plates, 1)
-
-    for clay_plate in clay_plates:
-        game_world.add_collision_pair('aiming_point:clay_plate', None, clay_plate)
+    runner = Runner()
+    game_world.add_object(runner, 1)
+    # 허들과 충돌 체크 추가 예정
 
 
 def finish():
@@ -40,17 +30,10 @@ def finish():
 
 def update():
     game_world.update()
+    game_world.handle_collisions()
 
 
 def draw():
     clear_canvas()
     game_world.render()
     update_canvas()
-
-
-def pause():
-    pass
-
-
-def resume():
-    pass
