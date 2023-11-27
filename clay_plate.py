0951583f9e63c814949a61c156996eff222432af
Rungbam(main) <marungbam@naver.com>
2023-11-27 23:18:27 +0900

    clay_plate 이미지 변경
    진짜 png파일로 변경

--- clay_plate.py
+++ clay_plate.py
@@ -5,32 +5,20 @@ import game_world
 import game_framework
 
 class Clay_plate:
-    image = None
+    # image = None
 
     def __init__(self):
-        if Clay_plate.image == None:
-            Clay_plate.image = load_image('clay_plate.png')
-        self.x, self.y = random.choice([0, 800]), random.randint(100, 350)
-        if self.x == 0:
-            self.dir = 1
-        elif self.x == 800:
-            self.dir = -1
-
+        self.x, self.y = random.choice([0, 800]), random.randint(350, 500)
+        self.image = load_image('clay_plate.png')
 
     def draw(self):
-        Clay_plate.image.draw(self.x, self.y, 50, 50)
+        self.image.draw(self.x, self.y, 50, 50)
         # draw_rectangle(*self.get_bb())
+        pass
 
     def update(self):
-        if self.dir == 1:
-            self.x += self.dir * 50 * game_framework.frame_time
-        elif self.dir == -1:
-            self.x += self.dir * 50 * game_framework.frame_time
-
-        if self.x < 25 or self.x > 800 - 25:
-            game_world.remove_object(self)
-
-        self.x = clamp(0, self.x, 800)
+        self.x += 1
+        pass
 
     def get_bb(self):
         pass
