94c36e7b72a25dc18b1c07d1b2fac147e69ea600
Rungbam(main) <marungbam@naver.com>
2023-12-01 23:52:46 +0900

    카누 속도 지정
    1.5km/h

--- Canoe.py
+++ Canoe.py
@@ -1,5 +1,5 @@
 from pico2d import get_time, load_image, load_font, clamp,  SDL_KEYDOWN, SDL_KEYUP, SDLK_SPACE, SDLK_UP, SDLK_DOWN
-form rock import Rock
+from rock import Rock
 import game_world
 import game_framework
 
@@ -15,24 +15,21 @@ def down_down(e):
 def down_up(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_UP
 
-# def space_down(e):
-#     return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_SPACE
-
 def time_out(e):
     return e[0] == 'TIME_OUT'
 
 
 # canoe speed
-PIXEL_PER_METER = (10.3 / 0.3)
-RUN_SPEED_KMPH = 20.0
+PIXEL_PER_METER = (1 / 1) # 1픽셀 1m
+RUN_SPEED_KMPH = 1.5 #1.5km/h
 RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
 RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
 RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
 
 # Canoe Action Speed
-TIME_PER_ACTION = 0.5
-ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
-FRAMES_PER_ACTION = 8
+# TIME_PER_ACTION = 0.5
+# ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+# FRAMES_PER_ACTION = 8
 
 
 class UP_and_DOWN:
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from pico2d import open_canvas, delay, close_canvas, hide_cursor
 import game_framework
 
-import play_mode_shooting as start_mode
+# import play_mode_hurdle as start_mode
+import play_mode_canoe as start_mode
+# import play_mode_shooting as start_mode
 # import logo_mode as start_mode
+
 
 open_canvas(800, 600)
 # hide_cursor()
