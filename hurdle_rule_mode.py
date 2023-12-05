fe5328501eda4ec0134b31bb080648510ef1501d
Rungbam(main) <marungbam@naver.com>
2023-12-05 22:10:45 +0900

    updated hurdle_rule_mode.py

--- hurdle_rule_mode.py
+++ hurdle_rule_mode.py
@@ -1,4 +1,4 @@
-from pico2d import load_image, get_events, clear_canvas, update_canvas
+from pico2d import load_image, get_events, clear_canvas, update_canvas, load_music
 from sdl2 import SDL_QUIT, SDL_KEYDOWN, SDLK_ESCAPE, SDLK_SPACE
 
 import game_framework
@@ -7,19 +7,21 @@ import play_mode_hurdle
 
 def init():
     global image
+    global bgm
     image = load_image('hurdle_rule.jpg')
+    bgm = load_music('Rules-Nintendo-Switch-Sports.mp3')
+    bgm.set_volume(32)
+    bgm.repeat_play()
 
 def finish():
     global image
+    global bgm
     del image
+    del bgm
 
 def handle_events():
     events = get_events()
     for event in events:
-        # if event.type == SDL_QUIT:
-        #     game_framework.quit()
-        # elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-        #     game_framework.quit()
         if (event.type, event.key) == (SDL_KEYDOWN, SDLK_SPACE):
             game_framework.change_mode(play_mode_hurdle)
 
